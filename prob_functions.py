495bc31eca5a6a06da42916f0d13bdc83aaa0e8f
Evan Kranzler <theelk801@gmail.com>
2019-02-25 00:40:14 -0500

    added a generator for opening hands

--- prob_functions.py
+++ prob_functions.py
@@ -14,3 +14,12 @@ def hypogeo(N1, n1, N2, n2, N3, n3):
     numerator = bino(N1, n1) * bino(N2, n2) * bino(N3, n3)
     denominator = bino(N1 + N2 + N3, n1 + n2 + n3)
     return numerator / denominator
+
+
+def hand_gen(bazaars, powders, other, hand_size=7):
+    for i in range(bazaars):
+        for j in range(powders):
+            for k in range(other):
+                if i + j + k == hand_size:
+                    yield i, j, k
+
