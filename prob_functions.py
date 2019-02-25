fde6097241da4db6a2e2983241ee0772dbe85ba3
Evan Kranzler <theelk801@gmail.com>
2019-02-25 12:06:31 -0500

    applied yapf

--- prob_functions.py
+++ prob_functions.py
@@ -32,8 +32,15 @@ def powder_gen(powders_in_hand, other_in_hand, to_put_under):
 
 
 @lru_cache(maxsize=None)
-def prob_of_keep(bazaars_in_hand, bazaars_in_deck, powders_in_hand, powders_in_deck, other_in_hand, other_in_deck,
-                 powders_on_bottom=0, other_on_bottom=0, mull_count=0):
+def prob_of_keep(bazaars_in_hand,
+                 bazaars_in_deck,
+                 powders_in_hand,
+                 powders_in_deck,
+                 other_in_hand,
+                 other_in_deck,
+                 powders_on_bottom=0,
+                 other_on_bottom=0,
+                 mull_count=0):
     # make sure that we're never trying to draw from a library which is too small
     if bazaars_in_deck + powders_in_deck + other_in_deck < 7:
         raise Exception('Not enough cards in library')
@@ -46,12 +53,21 @@ def prob_of_keep(bazaars_in_hand, bazaars_in_deck, powders_in_hand, powders_in_d
 
     # start with regular mulligan odds
     regular_mull = 0
-    for i, j, k in hand_gen(bazaars_in_deck, powders_in_deck + powders_on_bottom, other_in_deck + other_on_bottom):
+    for i, j, k in hand_gen(bazaars_in_deck,
+                            powders_in_deck + powders_on_bottom,
+                            other_in_deck + other_on_bottom):
         # find probability of hand being drawn
-        prob = hypogeo(bazaars_in_deck, i, powders_in_deck + powders_on_bottom, j, other_in_deck + other_on_bottom, k)
+        prob = hypogeo(bazaars_in_deck, i, powders_in_deck + powders_on_bottom,
+                       j, other_in_deck + other_on_bottom, k)
         # multiply by probability of hand leading to a keep
-        prob *= prob_of_keep(i, bazaars_in_deck, j, powders_in_deck + powders_on_bottom, k,
-                             other_in_deck + other_on_bottom, mull_count=mull_count + 1)
+        prob *= prob_of_keep(
+            i,
+            bazaars_in_deck,
+            j,
+            powders_in_deck + powders_on_bottom,
+            k,
+            other_in_deck + other_on_bottom,
+            mull_count=mull_count + 1)
         regular_mull += prob
 
     # if serum powder not in hand, or probability is already 1, we can stop
@@ -62,14 +78,17 @@ def prob_of_keep(bazaars_in_hand, bazaars_in_deck, powders_in_hand, powders_in_d
     all_mulls = [regular_mull]
 
     # iterate over all possible ways to put cards on the bottom
-    for powders_to_bottom, other_to_bottom in powder_gen(powders_in_hand, other_in_hand, mull_count):
+    for powders_to_bottom, other_to_bottom in powder_gen(
+            powders_in_hand, other_in_hand, mull_count):
         powders_left = powders_in_deck - powders_in_hand
         other_left = other_in_deck - other_in_hand
         powder_mull = 0
         for i, j, k in hand_gen(bazaars_in_deck, powders_left, other_left):
             prob = hypogeo(bazaars_in_deck, i, powders_left, j, other_left, k)
-            prob *= prob_of_keep(i, bazaars_in_deck, j, powders_left, k, other_left,
-                                 powders_on_bottom + powders_to_bottom, other_on_bottom + other_to_bottom, mull_count)
+            prob *= prob_of_keep(i, bazaars_in_deck, j, powders_left, k,
+                                 other_left,
+                                 powders_on_bottom + powders_to_bottom,
+                                 other_on_bottom + other_to_bottom, mull_count)
             powder_mull += prob
         if powder_mull == 1.0:
             return 1.0
@@ -93,29 +112,43 @@ def advice_string(key, mull_count):
         return 'Use Serum Powder.'
     a, b = key
     return f'Put {a} Serum Powder and {b} other card' + (
-        's' if b != 1 else ''
-    ) + ' on the bottom then use Serum Powder.'
+        's' if b != 1 else '') + ' on the bottom then use Serum Powder.'
 
 
-def action_to_take(powders_in_hand, other_in_hand, powders_in_deck, other_in_deck,
-                   powders_on_bottom=0, other_on_bottom=0, mull_count=0):
+def action_to_take(powders_in_hand,
+                   other_in_hand,
+                   powders_in_deck,
+                   other_in_deck,
+                   powders_on_bottom=0,
+                   other_on_bottom=0,
+                   mull_count=0):
     mull_dict = dict()
     regular_mull = 0
-    for i, j, k in hand_gen(4, powders_in_deck + powders_on_bottom, other_in_deck + other_on_bottom):
-        prob = hypogeo(4, i, powders_in_deck + powders_on_bottom, j, other_in_deck + other_on_bottom, k)
-        prob *= prob_of_keep(i, 4, j, powders_in_deck + powders_on_bottom, k,
-                             other_in_deck + other_on_bottom, mull_count=mull_count + 1)
+    for i, j, k in hand_gen(4, powders_in_deck + powders_on_bottom,
+                            other_in_deck + other_on_bottom):
+        prob = hypogeo(4, i, powders_in_deck + powders_on_bottom, j,
+                       other_in_deck + other_on_bottom, k)
+        prob *= prob_of_keep(
+            i,
+            4,
+            j,
+            powders_in_deck + powders_on_bottom,
+            k,
+            other_in_deck + other_on_bottom,
+            mull_count=mull_count + 1)
         regular_mull += prob
     mull_dict['regular'] = regular_mull
 
-    for powders_to_bottom, other_to_bottom in powder_gen(powders_in_hand, other_in_hand, mull_count):
+    for powders_to_bottom, other_to_bottom in powder_gen(
+            powders_in_hand, other_in_hand, mull_count):
         powders_left = powders_in_deck - powders_in_hand
         other_left = other_in_deck - other_in_hand
         powder_mull = 0
         for i, j, k in hand_gen(4, powders_left, other_left):
             prob = hypogeo(4, i, powders_left, j, other_left, k)
             prob *= prob_of_keep(i, 4, j, powders_left, k, other_left,
-                                 powders_on_bottom + powders_to_bottom, other_on_bottom + other_to_bottom, mull_count)
+                                 powders_on_bottom + powders_to_bottom,
+                                 other_on_bottom + other_to_bottom, mull_count)
             powder_mull += prob
         mull_dict[(powders_to_bottom, other_to_bottom)] = powder_mull
 
